9d8b190a4db6ae186b41a7e76fa94a4c9162c2b2
SunnyAVT <lxftm@hotmail.com>
2019-10-21 12:12:45 +0800

    update to display image in the sample code

--- avtcam/hi_avt.py
+++ avtcam/hi_avt.py
@@ -1,27 +1,28 @@
-import pymba
-import numpy
 import cv2
 import time
-import threading
 from avt_camera import AVTCamera
-import matplotlib.pyplot as plt
 
+'''
+# Display with image_widget in Jupyter
 import ipywidgets
 from IPython.display import display
 from utils import bgr8_to_jpeg
+'''
 
+update_flag = False
+image = None
 def main():
-    camera = AVTCamera(width=224, height=224, capture_width=1024, capture_height=768, capture_device=0)
+    global update_flag
+    global image
+    camera = AVTCamera(width=224, height=224, capture_width=640, capture_height=680, capture_device=0)
     image = camera.read()
 
     print("image: ", image.shape)
     print("callback return - ", camera.value.shape)
 
     cv2.imshow("cam", image)
-    c = cv2.waitKey(200)
-
-    plt.imshow(image)
-    plt.show()
+    c = cv2.waitKey(100)
+    update_flag = False
 
     '''
     # Display with image_widget in Jupyter
@@ -30,41 +31,40 @@ def main():
     display(image_widget)
     '''
 
-    def capture_frames() -> None:
-        while True:
-            if not camera.running:
-                break
-            print("thread ...")
-            cv2.imshow("cam-live", image)
-            c = cv2.waitKey(20)
-
     def update_image(change):
+        global update_flag
+        global image
         image = change['new']
-        # Display with image_widget in Jupyter
-        #image_widget.value = bgr8_to_jpeg(image)
+        update_flag = True
 
-        # the display in the callback will lead to trashed frame issue
-        print("app", image.shape)
-        #plt.imshow(image)
-        #plt.show()
+        '''
+        # Display with image_widget in Jupyter
+        image_widget.value = bgr8_to_jpeg(image)
+        '''
+
+        print("update:", image.shape, image[100, 100, 0])
+
+        # Warning: the display in the callback will lead to trashed frame issue
         #cv2.imshow("cam-live", image)
         #c = cv2.waitKey(5)
-
 
 
     camera.running = True
     camera.observe(update_image, names='value')
 
-    #app_thread = threading.Thread(target=capture_frames)
-    #app_thread.start()
-    # camera.unobserve(update_image, names='value')
-
     while True:
-        line = input()
-        if line == '\n' or line == '':
-            break
-        time.sleep(1)
-
+        if update_flag and camera.running:
+            cv2.imshow("cam-live", image)
+            #print("show", image[100, 100, 0])
+            c = cv2.waitKey(20)
+            update_flag = False
+        else:
+            #time.sleep(0.1)
+            c = cv2.waitKey(100)
+            if c == ord('q') or c == 27 :
+                break
+    time.sleep(1)
+    camera.unobserve(update_image, names='value')
     cv2.destroyAllWindows()
     print("program exit...")
 
